e68b15eeee3c0e4018473ec2e8df4f6849459e95
fbzr <fabricio.spb@gmail.com>
2020-02-21 09:12:08 -0800

    setup pagination components

--- starwars/src/components/Pagination.jsx
+++ starwars/src/components/Pagination.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
-const Pagination = () => {
+const Pagination = ({ postsPerPage, totalPosts }) => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
     return (
-        <div>
-            Pagination
-        </div>
+        <nav>
+            <ul>
+                {pageNumbers.map(number => (
+                    <li key={number}>
+                        <a href='!#'>
+                            {number}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     )
 }
 
