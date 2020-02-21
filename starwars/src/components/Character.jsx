6d2937e62d255729f8bbdd0a5ebbd7e9bcaba09d
fbzr <fabricio.spb@gmail.com>
2020-02-21 09:06:23 -0800

    style components with styled-components

--- starwars/src/components/Character.jsx
+++ starwars/src/components/Character.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const CharacterDiv = styled.div`
+    padding: 20px;
+    width: 16%;
+    background-color: rgba(255,255,255, 0.8);
+    border-radius: 15px;
+    margin-bottom: 20px;
+`
+
+const Name = styled.h3`
+    font-size: 2rem;
+    min-height: 70px;
+    padding-bottom: 20px;
+`
+
+const InfoText = styled.p`
+    font-size: 1.4rem;
+    margin-bottom: 7px;
+`
 
 const Character = ({ character }) => {
     const { name, height, mass, hair_color, skin_color, birth_year } = character;
     return (
-        <div>
-            <h3>{name}</h3>
-            <p>Birth year: {birth_year}</p>
-            <p>Height: {height}</p>
-            <p>Mass: {mass}</p>
-            <p>Hair: {hair_color}</p>
-            <p>Skin: {skin_color}</p>
-        </div>
+        <CharacterDiv>
+            <Name>{name}</Name>
+            <InfoText>Birth year: {birth_year}</InfoText>
+            <InfoText>Height: {height}</InfoText>
+            <InfoText>Mass: {mass}</InfoText>
+            <InfoText>Hair: {hair_color}</InfoText>
+            <InfoText>Skin: {skin_color}</InfoText>
+        </CharacterDiv>
     )
 }
 
