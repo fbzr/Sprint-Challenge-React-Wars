2dcf4e2722067a25a105d1090f95ef19bc5f7b53
fbzr <fabricio.spb@gmail.com>
2020-02-21 09:54:01 -0800

    implement and style pagination

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import starwarsData from './data/star-wars';
 import Header from './components/Header';
 import Display from './components/Display';
+import Pagination from './components/Pagination';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -10,19 +11,34 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [characters, setcharacters] = useState([]);
+  const [data, setData] = useState({
+      characters: [],
+      totalPosts: 0
+    });
   const [page, setpage] = useState(1);
+  const postsPerPage = 10;
 
   useEffect(() => {
-    starwarsData.getPeople()
-      .then(res => setcharacters(res.data.results))
+    starwarsData.getPeople(page)
+      .then(res => {
+        const { count, results } = res.data;
+        setData({
+          characters: results,
+          totalPosts: count
+        });
+      })
       .catch(err => console.log(err));
   }, [page]);
+
+  const changePage = (pageNumber) => {
+    setpage(pageNumber);
+  }
 
   return (
     <div className="App">
       <Header />
-      <Display characters={characters} />
+      <Display characters={data.characters} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={data.totalPosts} changePage={changePage} currentPage={page} />
     </div>
   );
 }
--- starwars/src/components/Pagination.jsx
+++ starwars/src/components/Pagination.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-const Pagination = ({ postsPerPage, totalPosts }) => {
+const LinksList = styled.ul`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`
+
+const PageLinkItem = styled.li`
+    font-size: 1.6rem;
+`
+
+const Link = styled.a`
+    border: 1px solid #dee2e6;
+    padding: .5rem .75rem;
+    height: 100%;
+    text-decoration: none;
+
+    background-color: ${ ({currentPage}) => currentPage ? `rgba(0,0,0, 0.8)` : `rgba(255,255,255, 0.8)` };
+    color: ${ ({currentPage}) => currentPage ? `rgb(255,255,255)` : `rgb(0,0,0)` }
+`
+
+const Pagination = ({ postsPerPage, totalPosts, changePage, currentPage }) => {
+    
     const pageNumbers = [];
 
     for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
@@ -9,15 +31,15 @@ const Pagination = ({ postsPerPage, totalPosts }) => {
 
     return (
         <nav>
-            <ul>
+            <LinksList>
                 {pageNumbers.map(number => (
-                    <li key={number}>
-                        <a href='!#'>
+                    <PageLinkItem key={number}>
+                        <Link onClick={() => changePage(number)} href='!#' currentPage={currentPage===number}>
                             {number}
-                        </a>
-                    </li>
+                        </Link>
+                    </PageLinkItem>
                 ))}
-            </ul>
+            </LinksList>
         </nav>
     )
 }
