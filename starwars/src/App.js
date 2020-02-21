4ca747216fc5514cd557e16bfa2b115ef7f307d9
fbzr <fabricio.spb@gmail.com>
2020-02-21 08:32:41 -0800

    create characters state and set it with the data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -8,9 +8,12 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setcharacters] = useState([]);
+  const [page, setpage] = useState(1);
+
   useEffect(() => {
     starwarsData.getPeople()
-      .then(res => console.log(res))
+      .then(res => setcharacters(res.data.results))
       .catch(err => console.log(err));
   }, []);
 
